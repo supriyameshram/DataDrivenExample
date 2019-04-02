934484f77b05d85e26646ec961341253fb64cffa
Supriya Meshram <supriya_meshram@persistent.co.in>
2019-04-02 18:19:45 +0530

    Data Driven Example

---
+++ src/utility/Constant.java
@@ -0,0 +1,16 @@
+package utility;
+
+public class Constant {
+
+	      public static final String URL = "http://www.store.demoqa.com";
+
+	      public static final String Username = "testuser_1";
+
+	      public static final String Password = "Test@123";
+
+	      public static final String Path_TestData = "D:/ToolsOA/DataDrivenExample/bin/testData";
+
+	      public static final String File_TestData = "TestData.xlsx";
+
+}
+
